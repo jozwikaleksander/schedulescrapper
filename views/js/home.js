d571dc86a80a5576e0b5ee76a5a98fb580730856
Aleksander Jozwik <aleksanderjozwik@elektronik.edu.pl>
2022-10-09 17:30:01 +0200

    Hiding disabled input elements

--- views/js/home.js
+++ views/js/home.js
@@ -2,7 +2,17 @@ const form = document.querySelector('form');
 
 const date = new Date();
 
+const currentLessonRadio = document.querySelector('#currentLesson');
 const timeInput = document.querySelector('#time-input');
+const dayInput = document.querySelector('#day');
+
+const timeLabel = document.querySelector('#time-label');
+const dayLabel = document.querySelector('#day-label');
+const dynamicContainer = document.querySelector('#dynamic-container');
+const scheduleRadio = document.querySelector('#schedule');
+
+currentLessonRadio.checked = false;
+scheduleRadio.checked = true;
 
 let hours = date.getHours();
 let minutes = date.getMinutes();
@@ -25,4 +35,20 @@ form.addEventListener('submit', function(e) {
     let day = form.elements.day.value;
 
     window.open(`?q=${option}&url=${name}&t=${time}&d=${day}`,'_self');
+});
+
+currentLessonRadio.addEventListener('change', () => {
+    if(currentLessonRadio.checked) {
+        dynamicContainer.className = 'more';
+    } else{
+        dynamicContainer.className = 'less';
+    }
+});
+
+scheduleRadio.addEventListener('click', () => {
+    if(scheduleRadio.checked) {
+        dynamicContainer.className = 'less';
+    } else{
+        dynamicContainer.className = 'more';
+    }
 });
