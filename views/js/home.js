6e4ce06f6b98ead938b9b07be14fb7ccdba5873e
Aleksander <aleksanderjozwik@elektronik.edu.pl>
2022-10-11 19:26:00 +0200

    Home form

--- views/js/home.js
+++ views/js/home.js
@@ -2,17 +2,15 @@ const form = document.querySelector('form');
 
 const date = new Date();
 
-const currentLessonRadio = document.querySelector('#currentLesson');
-const timeInput = document.querySelector('#time-input');
-const dayInput = document.querySelector('#day');
+// Current lesson inputs / containers
+const clRadio = document.querySelector('#currentLesson');
+const clTime = document.querySelector('#cl-time-input');
+const clDay = document.querySelector('#cl-day');
+const clContainer = document.querySelector('#cl-container');
 
-const timeLabel = document.querySelector('#time-label');
-const dayLabel = document.querySelector('#day-label');
-const currentLessonContainer = document.querySelector('#current-lesson-container');
 const scheduleRadio = document.querySelector('#schedule');
 const everyLessonRadio = document.querySelector('#everyLesson');
 const everyLessonContainer = document.querySelector('#every-lesson-container');
-
 
 let hours = date.getHours();
 let minutes = date.getMinutes();
@@ -24,7 +22,11 @@ if (hours < 10) {
     hours = '0' + hours;
 }
 
+clDay.value = date.getDay();
+
 timeInput.defaultValue = hours + ':' +minutes;
+
+$(clContainer).height(0);
 
 // Submiting form without reloading the website
 form.addEventListener('submit', function(e) {
@@ -32,29 +34,29 @@ form.addEventListener('submit', function(e) {
     let url = form.elements.url.value;
     let option = form.elements.query.value;
     let time = form.elements.time.value;
-    let day = dayInput.value;
+    let clDay = form.elements.clDay.value;
     let name = form.elements.name.value;
 
     if(name == ''&&url[url.length-1] == 'l'){
-        window.open(`?q=${option}&url=${url}&t=${time}&d=${day}`,'_self');
+        window.open(`?q=${option}&url=${url}&t=${time}&d=${cl-day}`,'_self');
     }
     else if (url[url.length-1] != 'l'){
         console.log(url[url.length-1]);
-        window.open(`?q=${option}&url=${url}&t=${time}&d=${day}&n=${name}`,'_self');
+        window.open(`?q=${option}&url=${url}&t=${time}&d=${cl-day}&n=${name}`,'_self');
     }
 });
 const radioChecked = (e) => {
     const id = e.id;
 
     if(id == "currentLesson") {
-        $(currentLessonContainer).animate({height:100},200);
+        $(clContainer).animate({height:100},200);
         $(everyLessonContainer).animate({height:0},200);
     } else if(id == "everyLesson") {
         $(everyLessonContainer).animate({height:30},200);
-        $(currentLessonContainer).animate({height:0},200);
+        $(clContainer).animate({height:0},200);
     }
     else{
         $(everyLessonContainer).animate({height:0},200);
-        $(currentLessonContainer).animate({height:0},200);
+        $(clContainer).animate({height:0},200);
     }
 }
